8af64424c93b84f5c2c22998ed446027255ace1a
Austin <ausitn.scott.j@gmail.com>
2020-01-24 06:26:51 -0600

    cleaned backend updated api endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://WeatherApp-env.ttvmtxr2pg.us-east-2.elasticbeanstalk.com'
+  baseUrl: 'https://fervent-roentgen-4b5a49.netlify.com/.netlify/functions/app'
+  // baseUrl: 'http://WeatherApp-env.ttvmtxr2pg.us-east-2.elasticbeanstalk.com'
 };
