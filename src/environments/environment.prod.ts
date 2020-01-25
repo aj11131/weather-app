46838f2bddeb90ffa70db268e73e86ca55100d7b
Austin <ausitn.scott.j@gmail.com>
2020-01-24 20:04:33 -0600

    updated outputPath

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://WeatherApp-env.ttvmtxr2pg.us-east-2.elasticbeanstalk.com'
+  baseUrl: 'https://weather-app11131.herokuapp.com/'
 };
